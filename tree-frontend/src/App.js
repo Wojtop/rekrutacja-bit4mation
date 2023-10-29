f4409f54bed7fcd9bc0062f96b0e44311dcf1bcc
Wojtek <wojtek.top.102@gmail.com>
2023-10-29 13:55:14 +0100

    [Frontend] Popup and displaying ID

--- tree-frontend/src/App.js
+++ tree-frontend/src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import { useCenteredTree } from "./helpers";
 import { useState, useEffect } from "react";
 
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
 const containerStyles = {
   width: "100vw",
   height: "100vh"
@@ -12,17 +15,45 @@ const containerStyles = {
 // Here we're using `renderCustomNodeElement` to represent each node
 // as an SVG `rect` instead of the default `circle`.
 const renderRectSvgNode = ({ nodeDatum, toggleNode }) => (
-  <g>
-    <circle r="10" onClick={toggleNode} />
-    <text fill="black" strokeWidth="1" x="20">
-      {nodeDatum.name}
-    </text>
-    {nodeDatum.attributes?.department && (
-      <text fill="black" x="20" dy="20" strokeWidth="1">
-        Department: {nodeDatum.attributes?.department}
+  <Popup trigger={
+    <g>
+      <circle r="10" onClick={() => { console.log("Node datum: ", nodeDatum) }} />
+      {nodeDatum.attributes != undefined &&
+      <text fill="black" strokeWidth="1" x="20">
+        {nodeDatum.name}
       </text>
-    )}
-  </g>
+  }
+      {nodeDatum.attributes?.id && 
+      (
+        <text fill="black" x="20" dy="20" strokeWidth="1">
+          ID: {nodeDatum.attributes?.id}
+        </text>
+      )}
+      {nodeDatum.attributes == undefined && 
+      (
+        <text fill="black" x="20" dy="20" strokeWidth="1">
+          Path sum: {nodeDatum.name}
+        </text>
+      )}
+    </g>
+  } modal nested>
+    {
+      close => (
+        <div className='modal'>
+          <div className='content'>
+            <h3>Value: {nodeDatum.name}</h3>
+            <h4>ID: {nodeDatum.attributes?.id}</h4>
+          </div>
+          <div>
+            <button onClick=
+              {() => close()}>
+              Close modal
+            </button>
+          </div>
+        </div>
+      )
+    }
+  </Popup>
 );
 
 function transalteData(treeResponse) {
@@ -43,7 +74,7 @@ function transalteData(treeResponse) {
   let visualTreeElems = new Map();
   references.forEach((value, key, map) => {
     // console.log("In map foreach. Key: ", key, " Value: ", value)
-    visualTreeElems.set(key.toString(), { name: value, attributes: { id: key }, children: [] })
+    visualTreeElems.set(key.toString(), { name: value.toString(), attributes: { "id": key.toString(), }, children: [] })
   })
   console.log("Visual tree elems: ", visualTreeElems)
   let childrenIds = [];   // list of all childrens ID. ID which is not here is a root
@@ -58,7 +89,7 @@ function transalteData(treeResponse) {
     })
   })
   leafs.forEach((sum, lastNodeId, map) => {
-      visualTreeElems.get(lastNodeId.toString()).children.push({name:sum, children:[]})
+    visualTreeElems.get(lastNodeId.toString()).children.push({ name: sum.toString(), children: [] })
   }
   )
 
@@ -84,32 +115,33 @@ export default function App() {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const getData = async()=>{fetch(`http://localhost:8080/tree/getTree`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json(); // Parse the response data as JSON 
-        } else {
-          throw new Error('API request failed');
-        }
-      })
-      .then((actualData) => {
-        actualData = transalteData(actualData)
-        console.log("Tree data: ", actualData)
-        setData(actualData);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setData(null);
-      })
-      .finally(() => {
-        setLoading(false);
-        console.log("Loading set to false")
-      });
+    const getData = async () => {
+      fetch(`http://localhost:8080/tree/getTree`)
+        .then((response) => {
+          if (response.ok) {
+            return response.json(); // Parse the response data as JSON 
+          } else {
+            throw new Error('API request failed');
+          }
+        })
+        .then((actualData) => {
+          actualData = transalteData(actualData)
+          console.log("Tree data: ", actualData)
+          setData(actualData);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setData(null);
+        })
+        .finally(() => {
+          setLoading(false);
+          console.log("Loading set to false")
+        });
       console.log("Data: ", data)
-  }
-  getData()
-}, []);
+    }
+    getData()
+  }, []);
 
   console.log("Data2: ", data)
   return (
